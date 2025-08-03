076c0af1e56b7e986b7a38cf3f4b3a5c3d6c5fc6
jessykiruba29 <jessykiruba2006@gmail.com>
2025-08-03 10:05:58 +0530

    done

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 import pdfplumber
 import os
-from backend.gemini import j_get_guidance, ask_insurance
+from backend.gemini import j_get_guidance, ask_insurance, categorize_message, extract_text_from_pdf
 
 app = FastAPI()
 
-from backend.gemini import j_get_guidance, ask_insurance, categorize_message,extract_text_from_pdf
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5174"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/chat")
 async def chat_with_bot(
@@ -15,7 +22,6 @@ async def chat_with_bot(
 ):
     try:
         if file and file.filename.endswith(".pdf"):
-           
             pdf_path = f"/tmp/{file.filename}"
             with open(pdf_path, "wb") as f:
                 f.write(await file.read())
